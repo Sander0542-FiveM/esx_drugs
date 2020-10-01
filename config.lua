68b9510023c471cf1e4915c555941764887279cf
Job <LordChunk@users.noreply.github.com>
2020-10-01 17:10:09 +0000

    Added Dutch locales and translations

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
-Config.Locale = 'en'
+Config.Locale = 'nl'
 
 Config.Delays = {
 	WeedProcessing = 1000 * 7
---
+++ locales/nl.lua
@@ -0,0 +1,31 @@
+Locales ['nl'] = {
+  -- weed
+  ['weed_pickupprompt'] = 'druk op ~INPUT_CONTEXT~ om de ~g~Cannabis~s~ plant te oogsten.',
+  ['weed_quitprocess'] = 'druk op ~INPUT_CONTEXT~ om te stoppen met ~g~Cannabisverwerking~s~.',
+  ['weed_inventoryfull'] = 'je hebt niet genoeg inventarisruimte voor ~g~Cannabis~s~.',
+  ['weed_processprompt'] = 'druk op ~INPUT_CONTEXT~ om ~g~Cannabisverwerking~s~ te starten.',
+  ['weed_processingstarted'] = '~g~Cannabis~s~ aan het verwerken naar ~g~Marihuana~s~...',
+  ['weed_processingfull'] = 'het verwerken is ~r~geannuleerd~s~ omdat je inventaris vol is!',
+  ['weed_processingenough'] = 'je moet ~b~2x~s~ ~g~Cannabis~s~ hebben om het te kunnen verwerken.',
+  ['weed_processed'] = 'je hebt ~b~2x~s~ ~g~Cannabis~s~ verwerkt naar ~b~1x~s~ ~g~Marihuana~s~',
+  ['weed_processingtoofar'] = 'het verwerken is ~r~geannuleerd~s~ omdat je het gebied hebt verlaten.',
+
+  -- drug dealer
+  ['dealer_prompt'] = 'druk op ~INPUT_CONTEXT~ om met de ~r~Drugsdealer~s~ te praten.',
+  ['dealer_title'] = 'drugsdealer',
+  ['dealer_item'] = '€%s',
+  ['dealer_notenough'] = 'je hebt niet genoeg om dit te kunnen verkopen!',
+  ['dealer_sold'] = 'je hebt ~b~%sx~s~ ~y~%s~s~ verkocht voor ~g~€%s~s~',
+
+  -- license
+  ['license_title'] = 'Je hebt een verwerkingslicentie nodig om dit product te verwerken. Koop je licentie hier.',
+  ['license_no'] = 'nee',
+  ['license_bought'] = 'je kocht de ~b~%s~s~ voor ~r~€%s~s~',
+  ['license_bought_fail'] = 'je hebt niet genoeg geld voor een ~b~%s~s~!',
+  ['license_weed'] = 'wietverwerkingslicentie',
+
+  -- blips
+  ['blip_weedfield'] = 'wietvelden',
+  ['blip_weedprocessing'] = 'wietverwerking',
+  ['blip_drugdealer'] = 'drugsdealer',
+}
